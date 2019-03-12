a0baff6613ef836c4c685d3f7be9b76775fdaeb7
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-03-11 20:52:51 -0500

    Pushing before handing in

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,67 @@
-var connection = requre("connection");
+var connection = require("../config/connection.js");
+
+//Use this function to generate "?" for mySQL
+function questionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+//Function to help with mySQL syntax
+function sqlMe(obj) {
+    var arr = [];
+
+    for (var key in obj) {
+        arr.push(key + "=" + obj[key]);
+    }
+    return arr.toString();
+};
 
 var orm = {
-    selectAll: function () {
-        var queryString = "SELECT * FROM burgers";
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+
         connection.query(queryString, function (err, result) {
             if (err) throw err;
             console.log(result);
+            cb(result);
+        });
+
+    },
+
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += questionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) throw err;
+            cb(result);
         });
     },
 
-    selectAll: function () {
-        var insertOne = "INSERT INTO burgers (burger_name, devoured) VALUES (?)" [req.body.burger];
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += sqlMe(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
         connection.query(queryString, function (err, result) {
             if (err) throw err;
-            console.log(res.json(result));
-        });
+            cb(result);
+        })
     }
-
 };
+
+module.exports = orm;
---
+++ public/assets/burger.js
@@ -0,0 +1,27 @@
+
+
+// $(function () {
+
+//     $(".change-devoured").on("click", function (event) {
+//         var id = $(this).data("id");
+
+//         var newDevouredState = {
+//             devoured: true
+//         };
+
+//         $.ajax("api/burgers/" + id, {
+//             type: "PUT",
+//             data: newDevouredState
+//         }).then(
+//             function () {
+//                 console.log("changed devoured status")
+//                 location.reload();
+//             }
+//         );
+//     });
+
+
+
+
+
+// })
